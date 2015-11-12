b90bf097d168390160deea90431749c02c6b35f6
Gavekort <Gavekort>
2015-11-12 13:34:37 +0100

    Added parsing-support for weechat-logs

--- Reader.py
+++ Reader.py
@@ -1,15 +1,26 @@
+import re
 
 class Reader(object):
     def __init__(self, path):
         self.sentences = []
-        self.f = open(path, 'r')
+        self.path = path
+        self.f = open(path, 'r', encoding='utf-8')
 
     @property
     def get_sentences(self):
         f = self.f
         sentences = self.sentences
 
-        for line in f:
-            sentences.append(line.split(' '))
+        if re.match('(.*).weechatlog', self.path) is not None: # if weechat
+            for line in f:
+                tmpsentences = line.split('\t')
+                datetime = tmpsentences[0]
+                user = tmpsentences[1]
+                msg = tmpsentences[2]
+                if re.match('<--|-->|--', user) is None:
+                    sentences.append(msg.split(' '))
+        else:
+            for line in f:
+                sentences.append(line.split(' '))
 
         return sentences
