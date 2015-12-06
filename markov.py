8a91d2250b87c8e460c21465baac1c088be8f29d
gavekort <gavekort>
2015-12-06 22:34:49 +0100

    Chain is now fully working, afaik

--- markov.py
+++ markov.py
@@ -16,25 +16,26 @@ class Markov(object):
     def add_chain(self, sentence):
         current = self.hash_table[hash("<s>")]  # Initialize start of chain
         for word in sentence:
-            try:
-                node = self.hash_table[hash(current.word)]  # see if we have the current word
-            except:  # if we don't have word
+
+            if hash(current.word) in self.hash_table:
+                node = self.hash_table[hash(current.word)]
+            else:
                 node = 0
 
             # Creation of next node
-            try:
-                _next = self.hash_table[hash(word)]
-            except:
-                _next = Node(word)  # create
+            if hash(word) in self.hash_table:
+                next_node = self.hash_table[hash(word)]
+            else:
+                next_node = Node(word)  # create
 
             if node is not 0:
-                current.add_link(_next, True)  # add word as seen
+                current.add_link(next_node)  # add word as seen
                 self.count += 1
 
-            self.hash_table[hash(_next.word)] = _next  # dump in HT
-            current = _next  # move up chain
+            self.hash_table[hash(next_node.word)] = next_node  # dump in HT
+            current = next_node  # move up chain
 
-        current.add_link(self.hash_table[hash("<\s>")], True)  # Add end link of chain
+        current.add_link(self.hash_table[hash("<\s>")])  # Add end link of chain
 
     def traverse(self, stoch):
         start = self.hash_table[hash("<s>")]
@@ -50,21 +51,18 @@ class Node(object):
         self.word = word
         self.links_ht = {}
 
-    def add_link(self, to, seen):
-        #print(self.word + " --> " + to.word)
-        try:
-            node = self.links_ht(hash(to.word))
-        except:
+    def add_link(self, to):
+        # print(self.word + " --> " + to.word)
+        if hash(to.word) in self.links_ht:
+            node = self.links_ht[hash(to.word)]
+        else:
             node = 0
 
-        print(node)
-
         if node is 0:
-            link = [1, to]  #count, and node it's pointing towards
+            link = [1, to]  # count, and node it's pointing towards
             self.links_ht[hash(to.word)] = link
         else:
-            link = self.links_ht(hash(node.word))
-            print("hi")
+            link = self.links_ht[hash(to.word)]
             link[0] += 1
 
 if __name__ == '__main__':
@@ -83,7 +81,7 @@ if __name__ == '__main__':
 
         print(m.count)
 
-        the = m.hash_table[hash("is")]
+        the = m.hash_table[hash("<s>")]
         links = the.links_ht
         for key in links:
             link = links[key]
