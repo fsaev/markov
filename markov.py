416df95129274c657d38b4612f3f4a00c22d282e
fredrsae <fredrsae@ifi.uio.no>
2016-05-20 23:13:49 +0200

    Comments and cleaning

--- markov.py
+++ markov.py
@@ -16,36 +16,36 @@ class Markov(object):
 
     def add_chain(self, sentence):
         previous = self.word_idx[hash("<s>")]
-        sentence.append("<\s>")
+        sentence.append("<\s>") # Add termination to end
         for word in sentence:
             if hash(word) in self.word_idx:  # if word in index
                 node = self.word_idx[hash(word)]
-                # print(node.word, " exists : ", hash(node.word))
             else:
                 node = Node(word)
                 self.word_idx[hash(word)] = node
-                # print(word, " was created : ", hash(word))
 
             previous.link_to(node)
-            previous = node
+            previous = node # Move up chain
 
     # eta_s: lower tolerance of observations
     def traverse(self, eta_s, minlen):
+        total_links = 0
+        iteration = 0
+        node = self.word_idx[hash("<s>")]
+        list_links = []
         if len(self.word_idx) is 0:
             print("Index empty - Build chain first")
             raise
-        total_links = 0
-        list_links = []
-        node = self.word_idx[hash("<s>")]
-        iteration = 0
-        while node is not self.word_idx[hash("<\s>")]:
-            for key in node.links:
+        while node is not self.word_idx[hash("<\s>")]: # While not end
+            for key in node.links: # Get all the links
                 link = node.links[key]
                 if link.count > int(eta_s):
+                    # Skip if end and iteration is not minlen
                     if link.to is self.word_idx[hash("<\s>")] \
                             and iteration < int(minlen):
                         continue
                     else:
+                        # Put links in a roulette
                         total_links = total_links + link.count
                         for i in range(0, link.count):
                             list_links.append(link)
@@ -53,9 +53,11 @@ class Markov(object):
                 print(node.word, "", end="")
             iteration = iteration + 1
             if total_links > 2:
+                # Roll roulette with 1 position per observation
                 node = list_links[randint(0, total_links - 1)].to
             else:
                 break
+            # Reset
             total_links = 0
             list_links = []
 
@@ -71,12 +73,9 @@ class Node(object):
         if hash(to) in self.links:  # if link already established
             link = self.links[hash(to)]
             link.count = link.count + 1  # increment link counta
-            # print(self.word, "->", to.word, "(", link.count, ")")
         else:
             link = Link(self, to)
             self.links[hash(to)] = link
-            # print(self.word, "->", to.word, " (new)")
-
 
 class Link(object):
     def __init__(self, fr, to):
@@ -85,6 +84,7 @@ class Link(object):
         self.count = 1
 
 if __name__ == '__main__':
+    # Args
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', default=None, nargs='?',
                         help='Destination to training-data')
@@ -95,6 +95,7 @@ if __name__ == '__main__':
     parser.add_argument('handles', default='n', nargs='?',
                         help='Add <s> </s> handles to output')
     args = parser.parse_args()
+
     if args.filename is not None:
         r = Reader(args.filename)
         sentences = r.get_sentences
@@ -103,6 +104,7 @@ if __name__ == '__main__':
         for sent in sentences:  # for each sentence
             sent[len(sent)-1] = sent[len(sent)-1].rstrip('\n')
             m.add_chain(sent)  # build chain out of words
+
         if args.handles is 'y':
             print("<s> ", end="")
             m.traverse(args.eta_s, args.minlen)
